a7137516d5278206d4173d06c850dac5fbe3e32b
Deena <mr.deena2k03@gmail.com>
2026-01-02 10:35:19 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -62,7 +62,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("import.meta.env.VITE_API_URL + "/api/contact/"", {
+    const res = await fetch("import.meta.env.VITE_API_URL + "api/contact/"", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
